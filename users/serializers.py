aefe959837f881ba68c03eff840b0daf2c1cfc27
Aleksey Koshkin <ekbdizzy@yandex.ru>
2022-06-19 14:37:17 +0600

    Fix conflict user model with Swagger

--- users/serializers.py
+++ users/serializers.py
@@ -9,5 +9,6 @@ class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
+        ref_name = 'Yanki User'
         model = User
         fields = ("first_name", "last_name", "email", "password")

bd183279ab9152594b4638ff8c1d2c1d69f9e0a2
Aleksey Koshkin <ekbdizzy@yandex.ru>
2022-07-02 18:35:14 +0600

    PIW Making TopMenu

--- react-front/src/components/TopMenu/TopMenu.jsx
+++ react-front/src/components/TopMenu/TopMenu.jsx
@@ -16,7 +16,8 @@ import {
   ViewGridIcon,
   XIcon,
 } from '@heroicons/react/outline';
-import { ChevronDownIcon } from '@heroicons/react/solid';
+import { UserIcon, ChevronDownIcon } from '@heroicons/react/solid';
+import { flex } from 'stylelint-order/rules/shorthandData';
 
 const LoginMenu = () => {
   return (
@@ -29,16 +30,33 @@ const LoginMenu = () => {
 
 const ProfileMenu = () => {
   return (
-    <Popover className="relative bg-white">
-      <Popover.Button className="px-8">
-        <span>Profile</span>
-      </Popover.Button>
-      <Popover.Panel as="div">
-        <div className="absolute mt-5 min-h-100">
-          Panel
+    <Menu className="relative bg-white" as="div">
+      <Menu.Button className="relative">
+        <div className='flex px-6 py-3 hover:bg-indigo-100'>
+          <UserIcon className="relative h-6"/>
+          <span>Profile</span>
         </div>
-      </Popover.Panel>
-    </Popover>
+      </Menu.Button>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-200"
+        enterFrom="opacity-0 translate-y-1"
+        enterTo="opacity-100 translate-y-0"
+        leave="transition ease-in duration-150"
+        leaveFrom="opacity-100 translate-y-0"
+        leaveTo="opacity-0 translate-y-1"
+      >
+
+        <Menu.Items className="absolute" as="ul">
+          <div className="bg-white border-2 border-indigo-200 p-6 my-2 ">
+            <Menu.Item as="li"><Link to="/profile/">Settings</Link></Menu.Item>
+            <Menu.Item as="li"><Link to="/profile/">Settings</Link></Menu.Item>
+            <Menu.Item as="li"><Link to="/profile/">Settings</Link></Menu.Item>
+            <Menu.Item as="li"><Link to="/profile/">Settings</Link></Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
   );
 };
 
